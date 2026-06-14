13f9a8cae3cdbad693f43a74b7b3f438a29e5b79
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2026-06-15 00:22:42 +0300

    refactoring

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -17,11 +17,18 @@ func TestWallet(t *testing.T) {
 	assertError := func(t testing.TB, got error, want string) {
 		t.Helper()
 		if got == nil {
-			t.Error("wanted error but didn't get one")
+			t.Fatal("wanted error but didn't get one")
 		}
 
 		if got.Error() != want {
 			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
 		}
 	}
 
@@ -33,7 +40,8 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
