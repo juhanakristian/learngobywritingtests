38aa3dfeec003dd7f21b4144103c52128c3c2e52
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2026-08-09 23:19:37 +0300

    update dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,6 +7,7 @@ type Dictionary map[string]string
 var (
 	ErrNotFound 	= errors.New("could not find the word you were looking for")
 	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrWordDoesNotExist = errors.New("cannot update word because it does not exist")
 )
 
 func (d Dictionary) Add(word, definition string) error {
@@ -32,4 +33,19 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }
